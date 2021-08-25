76f99e99f342ac4afaabd79225192fee29381cd9
zuhairzuberi <60561492+zuhairzuberi@users.noreply.github.com>
2021-08-24 22:53:19 -0400

    Update Deal_Or_No_Deal.py

--- Deal_Or_No_Deal.py
+++ Deal_Or_No_Deal.py
@@ -26,8 +26,57 @@ def safe_case(case):
                     print ()
                     print ("Invalid, please enter a number from 1 to 26")
         except ValueError:
-            print ("Please enter an int")    
-    
+            print ("Please enter an int")
+            
+def user_turns(case, cash):
+    global chosen_case
+    chosen_case = {}
+    case_count = 0
+    while True:
+        try:
+            if case_count == 0:
+                print ()
+                print ("You have to open 6 cases")
+            elif case_count == 6:
+                print ()
+                print ("You have to open 5 cases")
+            elif case_count == 11:
+                print ()
+                print ("You have to open 4 cases")
+            elif case_count == 15:
+                print ()
+                print ("You have to open 3 cases")
+            elif case_count == 18:
+                print ()
+                print ("You have to open 2 cases")
+            elif case_count == 20 or case_count == 21 or case_count == 22 or case_count == 23 or case_count == 24:
+                print ()
+                print ("You have to open 1 case")
+            print ()
+            case_choice = int(input("Please enter the number of the case you would like to eliminate: "))
+            print ()
+            if case_choice > 0 and case_choice < 27:
+                if case_choice in case:
+                    case_count += 1
+                    case.remove(case_choice)
+                    cash_case = choice(cash)
+                    print ("your case held: $" + str(cash_case))
+                    cash.remove(cash_case)
+                    chosen_case[case_choice] = cash_case
+                    if case_count == 6 or case_count == 11 or case_count == 15 or case_count == 18 or case_count == 20 or case_count == 21 or case_count == 22 or case_count == 23 or case_count == 24:
+                        Deal_or_NoDeal(cash, case, banker_offers)
+                        if Deal_or_noDeal == "D" or Deal_or_noDeal == "d":
+                            break
+                    if len(case) == 1:
+                        final_choice(case, cash)
+                        break
+                elif case_choice not in case:
+                    print ("Please enter an available case")
+            else:
+                print ("Please input a proper case")
+        except ValueError:
+            print ("Please enter an int ")
+            
 if __name__ == "__main__":
 
     user_name()
